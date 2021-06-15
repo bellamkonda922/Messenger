d80333e0aae87b29e835008708679114f93893b0
bellamkonda922 <bkartheek02@gmail.com>
2021-06-15 16:00:31 +0530

    Final commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/app";
+import "firebase/auth";
+
+
+export const auth = firebase.initializeApp( {
+    apiKey: "AIzaSyCGGrR-CEd2BLKSAnT7y9CpL82agQe9eZ4",
+  authDomain: "unichat-e15f2.firebaseapp.com",
+  projectId: "unichat-e15f2",
+  storageBucket: "unichat-e15f2.appspot.com",
+  messagingSenderId: "939869797559",
+  appId: "1:939869797559:web:002d73a2bc0021601b6132",
+  measurementId: "G-JS956SKDW3"
+  }).auth();
+
+  
